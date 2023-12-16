11dfb349ecc90a03b94d02bc2df46366a42b9459
Prathik Shetty <prathikkshetty15@gmail.com>
2023-12-17 01:07:52 +0530

    [utils]: db fixes

--- utils/ExtendedSiweMessage.tsx
+++ utils/ExtendedSiweMessage.tsx
@@ -4,13 +4,11 @@ export class ExtendedSiweMessage extends SiweMessage {
   username?: string;
   name?: string;
   pfp?: string;
-  isVerified?: boolean;
 
   constructor(param: any) {
     super(param);
     this.username = param.username;
     this.name = param.name;
     this.pfp = param.pfp;
-    this.isVerified = param.isVerified;
   }
 }
--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'Lens',
+      dbName: 'SoulBase',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
